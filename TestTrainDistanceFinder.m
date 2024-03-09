7d9c3aeb908977985c2c100b84e857ff3763cc03
Noah <nien@ucdavis.edu>
2024-03-08 17:33:43 -0800

    add checker function, still innacurate

---
+++ TestTrainDistanceFinder.m
@@ -0,0 +1,22 @@
+% TestTrainDistanceFinder.m
+
+% A function called from main to determine the closest match for every
+% audio samples in one object cell array to all the samples in another
+% object cell array
+function [minDistance, minIndex] = TestTrainDistanceFinder(inputArrayNum, testObj, trainObj)
+    numFiles = numel(trainObj);
+    distanceVector = zeros(numFiles, 1);
+    for k = 1:numFiles
+        if ~isa(trainObj{k}, 'double') && ~isa(testObj{inputArrayNum}, 'double') % Check if the object is of type/class WavFileObj
+            distanceVector(k, 1) = mean( ...
+                Distance2MelCeps(testObj{inputArrayNum}.MelCepstrumArray, trainObj{k}.MelCepstrumArray) ...
+                );
+        else
+            % Handle the case where the object is not of type/class WavFileObj
+            distanceVector(k, 1) = nan; % Set distance to very long
+        end
+    end
+    [minDistance, minIndex] = min(distanceVector);
+    fprintf('For test audio: %i\nIndex of the minimum element in the distance vector: %d\n It has a distance of: %f\n\n',inputArrayNum, minIndex, minDistance);
+end
+
